569167e1542384799b4ac02360711e0e2fb141ed
Haoyang Guo <82100936+HaoyangGuo@users.noreply.github.com>
2022-09-17 20:46:57 -0700

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,16 @@
 export class ApiError extends Error {
-	constructor(url, status) {
+	constructor(url: any, status: any) {
 		super(`'${url}' returned ${status}`);
 		if (Error.captureStackTrace) {
 			Error.captureStackTrace(this, ApiError);
 		}
 		this.name = "ApiError";
+		// @ts-ignore
 		this.status = status;
 	}
 }
 
-export async function fetchJson(url, options) {
+export async function fetchJson(url: any, options:any) {
 	const response = await fetch(url, options);
 	if (!response.ok) {
 		throw new ApiError(url, response.status);
